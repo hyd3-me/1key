ad1027082628082eb9d0752df904ad803e2e6fa9
sedg <hyde.rou@yandex.ru>
2025-03-06 20:29:34 +0300

    add func: check_password

--- gen_hash.py
+++ gen_hash.py
@@ -3,3 +3,6 @@ import bcrypt
 
 def generate_hash(pwd):
     return bcrypt.hashpw(pwd.encode('utf-8'), bcrypt.gensalt())
+
+def check_password(stored_hash, provided_password):
+    return bcrypt.checkpw(provided_password.encode(), stored_hash)
